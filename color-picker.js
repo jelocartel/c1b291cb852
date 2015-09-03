c043bfd8901a53276f95f43949f663689e9a01e7
Michał Budzyński <michal@virtualdesign.pl>
2015-09-03 23:16:12 +0200

    Revert "unstick spectrum and chosen list"
    This reverts commit 81c104e39be635baa52d15e89f775dcdc7a4081a.

--- color-picker.js
+++ color-picker.js
@@ -53,7 +53,7 @@ var Color = function(id, n, c, p, q) {
       qtyDivUp.classList.add('qty-update');
       if (!document.getElementById('qty-donut-spinner' + name)) {
         var spin = qtyDivUp.appendChild(document.createElement('img'));
-        spin.id = 'qty-donut-spinner' + name;'qty-donut-spinner' + name
+        spin.id = 'qty-donut-spinner' + name;
         spin.src = C1.dir + "modules/c1b291cb852/spinner.gif";
       }
     }
@@ -317,9 +317,7 @@ window.onload = function(){
     var chosenList = document.getElementsByClassName('c1-chosen-colors')[0];
     var stickAnchor = document.getElementsByClassName('c1-main-container')[0];
     var listTop = stickAnchor.offsetTop + 320; // I don't know why 320px but it's working
-    var listEnd = listTop + stickAnchor.offsetHeight;
-    console.log(listEnd);
-    if (windowTop > listTop && windowTop < listEnd) {
+    if (windowTop > listTop) {
       chosenList.classList.add('sticky');
       if (spectrum) {
         spectrum.classList.remove('c1-offscreen-hidden');
