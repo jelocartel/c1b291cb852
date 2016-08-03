429e6cc49bbd2e65971f49a21532e1b10eb5c093
developer <developer@GK-DEV.jelocartel>
2016-08-03 15:19:36 +0200

    added translation file

---
+++ translations/pl.php
@@ -0,0 +1,9 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{c1b291cb852}prestashop>c1b291cb852_aa8c5f6a882b7c427ff4f528ad4533c6'] = 'Modół wyboru kolorów';
+$_MODULE['<{c1b291cb852}prestashop>c1b291cb852_a238142adffdae8a1aa48c0a76c2ad3c'] = 'Wybierz kolory z podanego spektrum';
+$_MODULE['<{c1b291cb852}prestashop>c1b291cb852_876f23178c29dc2552c0b48bf23cd9bd'] = 'Czy napewno chcesz odinstalować?';
+$_MODULE['<{c1b291cb852}prestashop>c1b291cb852_7a9fd97db149bd598ddc53aa49f18396'] = 'Wybierz kolory';
+$_MODULE['<{c1b291cb852}prestashop>c1b291cb852_99092dde07100bb2533985fbc92f1343'] = 'Twoje kolory:';
